a3df02b02c0d9a50f473c325736a86e969069906
David B. Stocker <coffeemetalcode@gmail.com>
2021-02-23 20:10:44 -0500

    basic CRUD operations

---
+++ conference-demo/src/main/java/com/coffeemetalcode/conferencedemo/repositories/SpeakerRepository.java
@@ -0,0 +1,7 @@
+package com.coffeemetalcode.conferencedemo.repositories;
+
+import com.coffeemetalcode.conferencedemo.models.Speaker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
+}
